ae9e40812a9a7661997d6ec353d4477bbd2dfeaa
Yeraldine <yeraldinearianagr@gmail.com>
2023-06-04 14:26:34 -0300

    click al boton resumen

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+const cantDeTickets = document.getElementById('cantidad');
+const categoria = document.getElementById('categoria');
+const totalAPagar = document.getElementById('caja');
+const btnBorrar = document.getElementById('borrar');
+let btnResumen = document.getElementById("resumen");
+
+function clickResumen () {
+    console.log("El usuario hizo click")
+}
+
+btnResumen.onclick = clickResumen;
+
