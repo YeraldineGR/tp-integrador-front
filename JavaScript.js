91904c82dd1b04f5c741424f7fefc875fb1950ba
Yeraldine <yeraldinearianagr@gmail.com>
2023-06-04 21:53:31 -0300

    evento resumen

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,34 @@
-const cantDeTickets = document.getElementById('cantidad');
-const categoria = document.getElementById('categoria');
-const totalAPagar = document.getElementById('caja');
-const btnBorrar = document.getElementById('borrar');
-let btnResumen = document.getElementById("resumen");
 
-function clickResumen () {
-    console.log("El usuario hizo click")
-}
+const resumen = document.getElementById("buttongreenB2");
 
-btnResumen.onclick = clickResumen;
+const cantDeTickets = document.getElementById("cantidad");
 
+const selectCategoria = document.getElementById("ImputSelect");
+
+const texto = document.getElementById('total');
+
+resumen.addEventListener('click' , function(event){
+    event.preventDefault();
+
+    let cantidad = document.getElementById("cantidad").value;
+    let categoria = document.getElementById("ImputSelect").value;
+
+    let totalAPagar;
+    let subtotal = 200*cantidad;
+
+
+    if (categoria == 'Estudiante') {
+        totalAPagar = subtotal*0.2;
+        
+    } else if (categoria == 'Trainee') {
+        totalAPagar = subtotal*0.5;
+                
+    } else {
+        totalAPagar = subtotal*0.85;
+        console.log(`Total a pagar $ ${totalAPagar}`);
+    } 
+                          
+    texto.textContent += totalAPagar;
+    return;
+        
+});
